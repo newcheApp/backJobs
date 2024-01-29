dbf9e0f261189eda53ab450fd7139c916a50b701
Egemen <79070462+eqemenesen@users.noreply.github.com>
2024-01-29 21:54:02 +0300

    Update sciencedirect_ws.py
    Update sciencedirect_ws.py

--- webScraping/sciencedirect_ws.py
+++ webScraping/sciencedirect_ws.py
@@ -6,13 +6,10 @@ from pymongo import MongoClient
 uri = "mongodb://egemenNewcheAdmin:passNewche@localhost:27017/newcheDB"
 # Connect to the MongoDB client
 client = MongoClient(uri)
-
 # Select the database
 db = client['newcheDB']
-
 # Select the collection
 collection = db['unprocessedNews']
-
 # URL of ScienceAlert
 url = "https://www.sciencealert.com/"
 
