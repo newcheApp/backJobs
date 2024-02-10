0a9ac4b261fe008b9528c489ba0e2d578c63d42d
EgemenEsen <79070462+eqemenesen@users.noreply.github.com>
2024-02-10 03:10:12 +0300

    some print magic

--- main_backjobs.py
+++ main_backjobs.py
@@ -2,24 +2,24 @@
 import subprocess
 import sys
 
-print("Starting to Fetch News...\n")
+print("> Starting to Fetch News...\n")
 
-print("Fetching News in Science Alert...\n")
+print("> Fetching News in Science Alert...\n")
 subprocess.run([sys.executable, "webScraping\sciencealert_ws.py"])
 
 print("\n" + "-"*100)
-print("Fetching News in Space News...\n")
+print("> Fetching News in Space News...\n")
 subprocess.run([sys.executable, "webScraping\spacenews_ws.py"])
 
 print("\n" + "-"*100)
-print("Fetching News in Motorsports...\n")
+print("> Fetching News in Motorsports...\n")
 subprocess.run([sys.executable, "webScraping\motorsport_ws.py"])
 
 
 # And new websites before this line ^
 # Adding summaries
 print("\n" + "-"*100)
-print("Adding summaries of news...\n")
+print("> Adding summaries of news...\n")
 subprocess.run([sys.executable, "contentProcessing\summurization.py"])
 
 print("\nNews Fetching end.\n")
