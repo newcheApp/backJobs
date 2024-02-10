29b198edd2950aed70627387b3642a9735cc0df4
EgemenEsen <79070462+eqemenesen@users.noreply.github.com>
2024-02-10 03:08:46 +0300

    Summarization added to main

--- main_backjobs.py
+++ main_backjobs.py
@@ -15,4 +15,11 @@ print("\n" + "-"*100)
 print("Fetching News in Motorsports...\n")
 subprocess.run([sys.executable, "webScraping\motorsport_ws.py"])
 
+
+# And new websites before this line ^
+# Adding summaries
+print("\n" + "-"*100)
+print("Adding summaries of news...\n")
+subprocess.run([sys.executable, "contentProcessing\summurization.py"])
+
 print("\nNews Fetching end.\n")
